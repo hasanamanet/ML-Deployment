f52aaaa82339d30b07bb8923a74cda4dd4da3819
hasanamanet <61136006+hasanamanet@users.noreply.github.com>
2020-05-27 01:39:56 +0300

    Update app.py
    if deleted

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template,request
 import joblib
-
+import numpy as np
 
 app = Flask(__name__)
 
@@ -15,25 +15,12 @@ def tahmin_et():
 
     model = open("NB_gazete_model.pkl","rb")
     clf = joblib.load(model)
-    tahmin = ""
-
+    classes = np.array(["Magazin","Dünya","Spor","Siyaset","Kültür-Sanat","Teknoloji"])
     if request.method == "POST":
         text = request.form["haber_metni"]
         data = [text]
-        sonuc = clf.predict(data)
-    if sonuc == [0]:
-        tahmin = "Magazin"
-    elif sonuc == [1]:
-        tahmin = "Dünya"
-    elif sonuc == [5]:
-        tahmin = "Teknoloji"
-    elif sonuc == [3]:
-        tahmin = "Siyaset"
-    elif sonuc == [4]:
-        tahmin = "Kültür - Sanat"
-    else:
-        tahmin = "Spor"
-    return render_template("result.html", prediction = tahmin)
+        sonuc = clf.predict(data).astype(int)
+    return render_template("result.html", prediction = classes[sonuc])
 
 if __name__ == '__main__':
     app.run()
